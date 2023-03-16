a62b4a874f87921d6bc50be8436fd6ca4cf51cbd
tahsinkose <tahsinkose@yandex.com>
2023-03-17 00:44:01 +0300

    Update scripts to match the latest version.

--- px4_tutorial_pkg/env-hooks/custom_env.bash
+++ px4_tutorial_pkg/env-hooks/custom_env.bash
@@ -7,11 +7,11 @@ export ROS_TEST_RESULTS_DIR="$(pwd)/test_results"
 export ROS_LOG_DIR="$(pwd)/ros_logs"
 # PX4 stuff
 HOME_DIR=$(pwd)
-SRC_DIR=/PX4-Autopilot
-BUILD_DIR=$SRC_DIR/build/px4_sitl_default
+SRC_DIR=/px4_docker_tutorial/PX4-Autopilot
+PX4_GAZEBO_BUILD_DIR=$SRC_DIR/Tools/simulation/gazebo-classic/sitl_gazebo-classic/build
 
 # setup Gazebo env and update package path
-export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:${HOME_DIR}/devel/lib:${BUILD_DIR}/build_gazebo-classic
+export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:${HOME_DIR}/devel/lib:${PX4_GAZEBO_BUILD_DIR}
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:${SRC_DIR}/Tools/simulation/gazebo-classic/sitl_gazebo-classic/models
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${BUILD_DIR}:${BUILD_DIR}/build_gazebo-classic/devel/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${BUILD_DIR}:${PX4_GAZEBO_BUILD_DIR}
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$SRC_DIR:$SRC_DIR/Tools/simulation/gazebo-classic/sitl_gazebo-classic
--- run_container.sh
+++ run_container.sh
@@ -5,15 +5,14 @@ export UID=$(id -u)
 export GID=$(id -g)
 docker run -it --privileged \
     --user $UID:$GID \
-    -v ${PWD}/PX4-Autopilot:/PX4-Autopilot:rw \
+    -v ${PWD}:/px4_docker_tutorial:rw \
     -v ${PWD}/px4_tutorial_pkg:/px4_ws/src/px4_tutorial_pkg:rw \
     -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
-    --workdir="/quad_manip_ws" \
     --env="DISPLAY=$DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     -p 14556:14556/udp \
     --gpus all \
-    --workdir=/PX4-Autopilot \
+    --workdir=/px4_docker_tutorial/PX4-Autopilot \
     --name=px4-noetic-container px4-noetic bash
 
 xhost -
